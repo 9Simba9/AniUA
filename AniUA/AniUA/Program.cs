8c3f79b4acf0e21d36bc9da885f51171fee89210
Simba <107536029+9Simba9@users.noreply.github.com>
2026-05-24 22:24:24 +0300

    Add files via upload

---
+++ AniUA/AniUA/Program.cs
@@ -0,0 +1,25 @@
+﻿using AniUA.form;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AniUA
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new login());
+        }
+    }
+}
